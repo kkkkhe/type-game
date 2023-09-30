730eb1f54e9d1e17b5b12b3b4e0346548097f100
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-09-30 13:04:25 +0300

    feature(custom caret)

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@ type Text = {
   letters: Letter[],
   error: boolean
 }[]
+const words = ref<HTMLDivElement[]>([])
+const x = ref(0)
+const y = ref(0)
+const getY = () => {
+  const y = words.value[currentWordIndex.value]?.offsetTop
+  return y
+}
 const text = ref<Text>([
     {
       letters: [
@@ -43,9 +50,23 @@ const text = ref<Text>([
         {letter: 't', status: null},
       ],
       error: false
+    },
+    {
+      letters: [
+        {letter: 'T', status: null},
+        {letter: 'y', status: null},
+        {letter: 'p', status: null},
+        {letter: 'e', status: null},
+        {letter: 'S', status: null},
+        {letter: 'c', status: null},
+        {letter: 'r', status: null},
+        {letter: 'i', status: null},
+        {letter: 'p', status: null},
+        {letter: 't', status: null},
+      ],
+      error: false
     }
 ]);
-
 function pressLetter(event: KeyboardEvent){
   const key = event.key
   const letterIndex = currentLetterIndex.value
@@ -63,6 +84,7 @@ function pressLetter(event: KeyboardEvent){
     else {
       currentLetter.status = 'wrong'
     }
+    x.value += getLetterWidth(words.value[wordIndex]?.children[0])
     currentLetterIndex.value++ 
   }
   if(isBackSpace(event)){
@@ -73,17 +95,29 @@ function pressLetter(event: KeyboardEvent){
       else {
         currentWord[letterIndex - 1].status = null
       }
+      x.value -= words.value[wordIndex]?.children[0].clientWidth!
       currentLetterIndex.value--
     }
     else if(!letterIndex && wordIndex){
       currentWordIndex.value--
       const test = takeLastTypedLetter(text.value[wordIndex-1].letters)
-      currentLetterIndex.value =  test + 1
+      currentLetterIndex.value = test + 1
       if(text.value[wordIndex-1].error) text.value[wordIndex-1].error = false
+      //to separate function
+      const element = words.value[wordIndex-1].children
+      const children  = Array.from(element)
+      for(let letterIndex = children.length; letterIndex > 0; letterIndex--){
+        const letter = children[letterIndex] as HTMLElement
+        if(letter && !letter?.className.includes('notEntered')){
+          const wordOffset = words.value[wordIndex-1].offsetLeft
+          x.value = wordOffset + letter.offsetLeft + letter.offsetWidth
+          break
+        }
+      }
     }
   }
   if(isCtrlBackSpace(event)){
-    if(!wordIndex) return
+    if(!wordIndex && !letterIndex) return
     if(!letterIndex){
       currentWordIndex.value--
     }
@@ -91,6 +125,7 @@ function pressLetter(event: KeyboardEvent){
     const defaultText = wordWithoutExtra.map(({letter}) => ({letter, status: null}))
     text.value[currentWordIndex.value].letters = defaultText
     currentLetterIndex.value = 0
+    x.value = words.value[currentWordIndex.value]?.offsetLeft
     if(text.value[currentWordIndex.value].error) text.value[currentWordIndex.value].error = false
   }
   if(isSpace(event.code)){
@@ -100,7 +135,11 @@ function pressLetter(event: KeyboardEvent){
     }
     currentWordIndex.value++
     currentLetterIndex.value = 0
+    x.value = words.value[currentWordIndex.value]?.offsetLeft
   }
+}
+function getLetterWidth(letter: HTMLElement | Element){
+  return letter.getBoundingClientRect().width
 }
 function isLetter(key: string){
   if(/^[a-z]$/i.test(key)) return true
@@ -114,7 +153,6 @@ function isCtrlBackSpace(event: KeyboardEvent){
   if(event.key == 'Backspace' && event.ctrlKey) return true
   return false
 }
-
 function isSpace(key: string){
   if(key === 'Space') return true
   return false
@@ -136,11 +174,34 @@ function takeLastTypedLetter(letters: Letter[]){
 document.addEventListener('keydown', pressLetter)
 </script>
 <template>
-  <div style="display: flex; gap: 10px">
-    <div v-for="({ letters, error }) in text">
-      <span :class="{error, notEntered: !status, correctLetter: status == 'corrent', wrongLetter: status == 'wrong', extraLetter: status == 'extra'}" v-for="{letter, status} in letters">
-        {{ letter }}
-      </span>
+  <div style="font-size: 24px;">
+    <div style="display: flex; gap: 1em; position: relative">
+    <div 
+      :style="{
+        top: getY() + 'px',
+        left: x + 'px'
+      }"
+      class="caret"></div>
+      <div 
+        ref="words"
+        class="word"
+        :class="{
+          error,
+          active: wordIndex == currentWordIndex
+        }"
+        v-for="({ letters, error }, wordIndex) in text">
+        <div 
+          v-for="{letter, status} in letters" 
+          class="letter"
+          :class="{
+            notEntered: !status, 
+            correctLetter: status == 'corrent', 
+            wrongLetter: status == 'wrong', 
+            extraLetter: status == 'extra',
+            }">
+          {{ letter }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -149,8 +210,42 @@ document.addEventListener('keydown', pressLetter)
 .correctLetter {
   color: rgb(25, 124, 25);
 }
+.caret {
+  position: absolute;
+  width: 3px;
+  height: 29px;
+  background-color: rgb(24, 130, 156);
+  border-radius: 5px;
+  animation: pulse 1.2s infinite;
+  transition-duration: 0.1s;
+  z-index: 10;
+}
+.word {
+  display: flex;
+  user-select: none;
+  position: relative
+}
+.letter {
+  height: 1.2em;
+  display: flex;
+  align-items: center;
+}
 .error {
   border-bottom: 1px solid red;
+}
+.leftIndicator {
+  border-left: 1px solid pink;
+}
+@keyframes pulse {
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+}
+.rightIndicator:last-child {
+  border-right: 1px solid pink;
 }
 .notEntered {
   color: grey
